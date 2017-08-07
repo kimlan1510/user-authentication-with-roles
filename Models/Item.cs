a2beefc10790e6d92d4861a6301016cbd794d48c
Olena Kuchko <kuchkoel@gmail.com>
2017-08-06 17:22:25 -0700

    add todo functionality

---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthentication.Models
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
